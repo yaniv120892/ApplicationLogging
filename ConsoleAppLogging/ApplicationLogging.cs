93f7c87a1f54fb218a73edf784c36236bcea9b8c
Yaniv Daye <yaniv120892@gmail.com>
2020-07-08 15:27:01 +0300

    First commit

---
+++ ConsoleAppLogging/ApplicationLogging.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace ConsoleAppLogging
+{
+    public static class ApplicationLogging
+    {
+        public static ILogger CreateLogger<T>() =>
+            s_loggerFactory.CreateLogger<T>();
+
+        private static readonly ILoggerFactory s_loggerFactory = LoggerFactory.Create(AddNLog);
+
+        private static void AddNLog(ILoggingBuilder builder)
+        {
+            var configJson = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            LogManager.Configuration = new NLogLoggingConfiguration(configJson.GetSection("NLog"));
+            builder.AddNLog();
+        }
+    }
+}
---
+++ ConsoleAppLogging/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConsoleAppLogging
+{
+    public class Program
+    {
+        private static readonly ILogger s_logger = ApplicationLogging.CreateLogger<Program>();
+
+        static void Main()
+        {
+            s_logger.LogCritical("LogCritical");
+            s_logger.LogDebug("LogDebug");
+            s_logger.LogError("LogError");
+            s_logger.LogInformation("LogInformation");
+            s_logger.LogTrace("LogTrace");
+            s_logger.LogWarning("LogWarning");
+        }
+    }
+}
